37c55ac3cc161b94523ada5d41924592ec506ef3
EricPC <eric.carlottoitapiruba@gmail.com>
2021-04-25 23:37:14 -0300

    Finalizando as aulas da NLW 5

---
+++ lib/result/result_page.dart
@@ -0,0 +1,96 @@
+import 'package:devquiz/challenge/widgets/next_button/next_butto_widget.dart';
+import 'package:devquiz/core/app_text_styles.dart';
+import 'package:devquiz/core/core.dart';
+import 'package:flutter/material.dart';
+import 'package:share_plus/share_plus.dart';
+
+class ResultPage extends StatelessWidget {
+  final String title;
+  final int lenght;
+  final int result;
+
+  const ResultPage(
+      {Key? key,
+      required this.title,
+      required this.lenght,
+      required this.result})
+      : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.maxFinite,
+        padding: EdgeInsets.only(top: 100),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset(AppImages.trophy),
+            Column(
+              children: [
+                Text(
+                  "Parabéns!",
+                  style: AppTextStyles.heading40,
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Text.rich(
+                  TextSpan(
+                      text: "Você concluiu ",
+                      style: AppTextStyles.body,
+                      children: [
+                        TextSpan(
+                          text: "\n$title",
+                          style: AppTextStyles.bodyBold,
+                        ),
+                        TextSpan(
+                          text: "\ncom $result de $lenght acertos.",
+                          style: AppTextStyles.body,
+                        ),
+                      ]),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+            Column(
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 68),
+                        child: NextButtonWidget.purple(
+                            label: "Compartilhar",
+                            onTap: () {
+                              Share.share(
+                                  'DevQuiz NLW 5 - Flutter: Resultado do Quiz: $title\nObtive: ${((result / lenght) * 100).roundToDouble()}% aproveitamento!');
+                            }),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 24),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 68),
+                        child: NextButtonWidget.white(
+                          label: "Voltar ao início",
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
